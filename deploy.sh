3dd1a6e7bd8a5d86924c6bfe17f5b9bbdd81a5c5
Yukitaka Maeda <yumaeda@gmail.com>
2020-06-07 14:39:14 +0900

    Exclude config files from deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 rm .DS_Store
 rm ./*/.DS_Store
 
-aws s3 cp ./ s3://tokyo-takeout.com/ --recursive --exclude "dist/" --exclude "node_modules/" --exclude ".git/"  --exclude "README.md" --exclude "deploy.sh" --exclude ".gitignore" --exclude ".git/*" --exclude "package.json"
+aws s3 cp ./ s3://tokyo-takeout.com/ --recursive --exclude "dist/" --exclude "node_modules/" --exclude ".git/"  --exclude "README.md" --exclude "deploy.sh" --exclude ".gitignore" --exclude ".git/*" --exclude "package.json" --exclude "webpack.config.js" --exclude "tsconfig.json"
