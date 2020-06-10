cf4c74ca82fef653da384406c83b63a0d9bdb884
Yukitaka Maeda <yumaeda@gmail.com>
2020-06-10 21:35:05 +0900

    Replace top page with React-based page

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -5,7 +5,33 @@ import * as React from 'react'
 
 const Root: React.FC<{}> = () => {
     return (
-        <div className="contents"></div>
+        <>
+            <header>
+                <h1 className="header-label">東京テイクアウト</h1>
+                <img className="header-image" src="https://cdn.tokyo-hideaway.com/images/cover.jpg" alt="東京テイクアウト・カバー" />
+            </header>
+            <div className="contents">
+                <p>
+                    東京でもようやく緊急事態宣言が解除されました。<br/>
+                    営業を再開したお店も多いですが、新型コロナウイルスへの懸念からテイクアウトを続けるお店も多いようです。<br/>
+                    消費者としては、<a href="https://demae-can.com/" rel="nofollow noopener">出前館</a>や<a href="https://about.ubereats.com/ja/" rel="nofollow noopener">Uber Eats</a>のような出前の方が手軽ですが、手数料や初期費用がかかるため導入を躊躇するお店は多いと思います。<br/><br/>
+                    このサイトでは、そのようなお悩みを抱えていると思われる東京のお店を私の方で勝手に紹介していきます。<br/>
+                </p>
+                <ul className="town-list">
+                    <li className="list-item"><a href="./ikebukuro/index.html">池袋</a></li>
+                    <li className="list-item"><a href="./kagurazaka/index.html">神楽坂</a></li>
+                    <li className="list-item"><a href="./kanda/index.html">神田</a></li>
+                    <li className="list-item"><a href="./kyodo/index.html">経堂</a></li>
+                    <li className="list-item"><a href="./shibuya/index.html">渋谷</a></li>
+                    <li className="list-item"><a href="./meguro/index.html">目黒</a></li>
+                </ul>
+                <p>
+                    <strong>[免責について]</strong><br /><br />
+                    本サイトを利用する事により被った、いかなる被害や損害についても当サイトの管理者は一切の責任を負いません。<br />
+                </p>
+            </div>
+            <footer className="footer">&copy; 2020 東京テイクアウト</footer>
+        </>
     )
 }
 
