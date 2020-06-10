b59f6ca0b2620e5ca0e6a117b2b33083aa6e078d
Yukitaka Maeda <yumaeda@gmail.com>
2020-06-10 21:19:17 +0900

    Add shops and CloudFront invalidation command to deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 rm .DS_Store
 rm ./*/.DS_Store
 
-aws s3 cp ./ s3://tokyo-takeout.com/ --recursive --exclude "dist/" --exclude "node_modules/" --exclude ".git/"  --exclude "README.md" --exclude "deploy.sh" --exclude ".gitignore" --exclude ".git/*" --exclude "package.json" --exclude "webpack.config.js" --exclude "tsconfig.json"
+aws s3 cp ./ s3://tokyo-takeout.com/ --recursive --exclude "dist/*" --exclude "node_modules/*" --exclude ".git/"  --exclude "README.md" --exclude "deploy.sh" --exclude ".gitignore" --exclude ".git/*" --exclude "package.json" --exclude "package-lock.json" --exclude "webpack.config.js" --exclude "tsconfig.json"
+aws cloudfront create-invalidation --distribution-id E37SHWAC97A2BI --paths "/*"
