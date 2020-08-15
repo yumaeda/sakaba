cd3ec760a82a1b3b54848d3aae9c987590c5ccef
Yukitaka Maeda <yumaeda@gmail.com>
2020-08-15 18:36:53 +0900

    Revert the change

--- src/components/pages/AreaPage.tsx
+++ src/components/pages/AreaPage.tsx
@@ -39,8 +39,7 @@ const AreaPage: React.FC<{ match: any }> = (props) => {
                 .then(res => res.json())
                 .then(
                     (data) => {
-                        setShops(JSON.parse(data.body))
-                         // match.params.area
+                        setShops(JSON.parse(data.body)[match.params.area])
                     },
                     (error: Error) => { setError(error); }
                 )
