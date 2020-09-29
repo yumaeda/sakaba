aaa8b2cc78c3bdb7e19ece3ddbbcef1c343e83b3
Yukitaka Maeda <yumaeda@gmail.com>
2020-09-29 21:27:00 +0900

    Update webp generation script to automatically assign file names

--- generate_webp.sh
+++ generate_webp.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-for FILE in *.jpg
+curIndex=1
+for FILE in *.{jpg,JPG}
 do
-    newfile="$(echo ${FILE} | sed -e 's/.jpg//')" 
+    newfile=$(printf "%07d" "$curIndex")
     cwebp ${FILE} -o "${newfile}.webp"
     cwebp -resize 0 150 ${FILE} -o "${newfile}_thumbnail.webp"
+    mv -i ${FILE} "${newfile}.jpg"
+    let curIndex=curIndex+1
 done
-
