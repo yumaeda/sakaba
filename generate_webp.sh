2bd1f609328a35c054794284607abb9fe3bd163e
Yukitaka Maeda <yumaeda@gmail.com>
2020-09-29 21:50:59 +0900

    Bug Fix: File name is not sequencial when the file extensions jpg and JPG coexsit

--- generate_webp.sh
+++ generate_webp.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 curIndex=1
-for FILE in *.{jpg,JPG}
+for FILE in *.{JPG,jpg}
 do
-    newfile=$(printf "%07d" "$curIndex")
-    cwebp ${FILE} -o "${newfile}.webp"
-    cwebp -resize 0 150 ${FILE} -o "${newfile}_thumbnail.webp"
-    mv -i ${FILE} "${newfile}.jpg"
-    let curIndex=curIndex+1
+    if [ -f ${FILE} ]; then
+        newfile=$(printf "%07d" "$curIndex")
+        cwebp ${FILE} -o "${newfile}.webp"
+        cwebp -resize 0 150 ${FILE} -o "${newfile}_thumbnail.webp"
+        mv ${FILE} "${newfile}.jpg"
+        let curIndex=curIndex+1
+    fi
 done
