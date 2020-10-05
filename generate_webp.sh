899ebd2f76e47a93ab2918d1174922fe7cf9093c
Yukitaka Maeda <yumaeda@gmail.com>
2020-10-05 14:55:20 +0900

    Add command to generate thumbnail jpg

--- generate_webp.sh
+++ generate_webp.sh
@@ -8,6 +8,9 @@ do
         cwebp ${FILE} -o "${newfile}.webp"
         cwebp -resize 0 150 ${FILE} -o "${newfile}_thumbnail.webp"
         mv ${FILE} "${newfile}.jpg"
+        cp "${newfile}.jpg" "${newfile}_thumbnail.jpg"
+        sips -z 150 200 "${newfile}_thumbnail.jpg"
+
         let curIndex=curIndex+1
     fi
 done
