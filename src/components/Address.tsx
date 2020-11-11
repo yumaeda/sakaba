9e00d7cfed3b19e1cb54ce54697c8ceacca2b4c5
Yukitaka Maeda <yumaeda@gmail.com>
2020-11-11 13:43:08 +0900

    Replace map text with icon

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -14,7 +14,12 @@ const Address: React.FC<Props> = (props) => {
 
     return (
         <div className="restaurant-adress">
-            <a href={`https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}`} target="_blank" title={text}>[Map]</a>
+            <a href={`https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}`} target="_blank" title={text}>
+                <picture>
+                    <source type="image/webp" media="(min-width: 150px)" srcSet="https://tokyo-takeout.com/images/map.webp" />
+                    <img src="https://tokyo-takeout.com/images/map.png" className="map-image" alt="Google Map" />
+                </picture>
+            </a>
         </div>
     )
 }
