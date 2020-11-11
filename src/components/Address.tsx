71d90d5346544abdc073b1898aadc7b8126e1cfb
Yukitaka Maeda <yumaeda@gmail.com>
2020-11-11 13:49:30 +0900

    Adjust map image style

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -13,14 +13,14 @@ const Address: React.FC<Props> = (props) => {
     const { text, latitude, longitude } = props
 
     return (
-        <div className="restaurant-adress">
+        <>
             <a href={`https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}`} target="_blank" title={text}>
                 <picture>
                     <source type="image/webp" media="(min-width: 150px)" srcSet="https://tokyo-takeout.com/images/map.webp" />
                     <img src="https://tokyo-takeout.com/images/map.png" className="map-image" alt="Google Map" />
                 </picture>
             </a>
-        </div>
+        </>
     )
 }
 
