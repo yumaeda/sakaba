f364450f87ee56a4450c3a7077b4447e12c2788b
Yukitaka Maeda <yumaeda@gmail.com>
2021-01-27 13:22:48 +0900

    Change the Webpack mode to production

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path')
 
 module.exports = {
-  // mode: 'production',
-  mode: 'development',
+  mode: 'production',
+  // mode: 'development',
   entry: './src/index.tsx',
   module: {
       rules: [
