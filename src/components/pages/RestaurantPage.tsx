af20f36f22f3ef85bc613e4b0cc62a920f9f57d0
Yukitaka Maeda <yumaeda@gmail.com>
2021-07-17 09:05:59 +0900

    Add cocktail category

--- src/components/pages/RestaurantPage.tsx
+++ src/components/pages/RestaurantPage.tsx
@@ -6,15 +6,17 @@ import Restaurant from '../../interfaces/Restaurant'
 import Footer from '../Footer'
 
 const RestaurantPage: React.FC<{ match: any }> = (props) => {
-    /*
     const cocktailTypes = [
-        'WINE BASE',
-        'LIQUEUR BASE',
-        'TEQUILA BASE',
-        'BRANDY BASE',
-        'WHISKY BASE'
+        { label: 'WINE BASE', type: 0 },
+        { label: 'LIQUEUR BASE', type: 1 },
+        { label: 'TEQUILA BASE', type: 2 },
+        { label: 'BRANDY BASE', type: 3 },
+        { label: 'WHISKY BASE', type: 4 },
+        { label: 'GIN BASE', type: 5 },
+        { label: 'VODKA BASE', type: 6 },
+        { label: 'RUM BASE', type: 7 }
     ]
-    */
+
     const cocktails = [
         { name: 'Kir', nameJa: 'キール', price: 700, type: 0 },
         { name: 'Spritzer', nameJa: 'スプリッツアー', price: 700, type: 0 },
@@ -47,7 +49,29 @@ const RestaurantPage: React.FC<{ match: any }> = (props) => {
         { name: 'New York', nameJa: 'ニューヨーク', price: 900, type: 4 },
         { name: 'Hunter', nameJa: 'ハンター', price: 900, type: 4 },
         { name: 'Manhattan', nameJa: 'マンハッタン', price: 900, type: 4 },
-        { name: 'Rob Roy', nameJa: 'ロブ・ロイ', price: 900, type: 4 }
+        { name: 'Rob Roy', nameJa: 'ロブ・ロイ', price: 900, type: 4 },
+        { name: 'Gin & Tonic', nameJa: 'ジン・トニック', price: 650, type: 5 },
+        { name: 'Negroni', nameJa: 'ネグローニ', price: 800, type: 5 },
+        { name: 'White Lady', nameJa: 'ホワイトレディ', price: 900, type: 5 },
+        { name: 'Blue Moon', nameJa: 'ブルームーン', price: 900, type: 5 },
+        { name: 'Gimlet', nameJa: 'ギムレット', price: 900, type: 5 },
+        { name: 'Gimlet Highball', nameJa: 'ギムレット・ハイボール', price: 950, type: 5 },
+        { name: 'Martini', nameJa: 'マティーニ', price: 900, type: 5 },
+        { name: 'Gin Fizz', nameJa: 'ジン・フィズ', price: 900, type: 5 },
+        { name: 'French 75', nameJa: 'フレンチ７５', price: 1200, type: 5 },
+        { name: 'Godmother', nameJa: 'ゴッド・マザー', price: 800, type: 6 },
+        { name: 'Salty Dog', nameJa: 'ソルティ・ドッグ', price: 850, type: 6 },
+        { name: 'Kami-Kaze', nameJa: 'カミカゼ', price: 850, type: 6 },
+        { name: 'Bloody Ceasar', nameJa: 'ブラッディー・シーザー', price: 850, type: 6 },
+        { name: 'Moscow Mule', nameJa: '5種のハーブとスパイスのモスコミュール', price: 900, type: 6 },
+        { name: 'Yuki-Guni', nameJa: '雪国', price: 900, type: 6 },
+        { name: 'Cuba Libre', nameJa: 'キューバ・リブレ', price: 800, type: 7 },
+        { name: 'Sol Cubano', nameJa: 'ソル・クバーノ', price: 850, type: 7 },
+        { name: 'Mojito', nameJa: 'モヒート', price: 850, type: 7 },
+        { name: 'Boston Cooler', nameJa: 'ボストン・クーラー', price: 850, type: 7 },
+        { name: 'Sky Diving', nameJa: 'スカイ・ダイビング', price: 900, type: 7 },
+        { name: 'X.Y.Z.', nameJa: 'エックス・ワイ・ズィー', price: 900, type: 7 },
+        { name: 'Daiquiri', nameJa: 'ダイキリ', price: 900, type: 7 }
     ]
 
     const { match } = props
@@ -92,24 +116,32 @@ const RestaurantPage: React.FC<{ match: any }> = (props) => {
                     <h1 className="header-label">{restaurants ? restaurants[0].name : 'N/A'}</h1>
                 </header>
                 <div className="contents">
-                    <h4 className="cocktail-page-title">COCKTAIL</h4>
-                    <ul className="cocktail-list">
                     {
-                        cocktails.map((cocktail) => {
+                        cocktailTypes.map((cocktailType) => {
                             return (
-                                <li className="cocktail-item">
-                                    <div className="cocktail-name-cell">
-                                        <span className="cocktail-name">{cocktail.name}</span>
-                                        <br />
-                                        <span className="cocktail-name-ja">{cocktail.nameJa}</span>
-                                    </div>
-                                    <div className="cocktail-price-cell">{`${cocktail.price.toLocaleString()} yen`}</div>
-                                </li>
+                                <div>
+                                    <h4>{cocktailType.label}</h4>
+                                    <ul className="cocktail-list">
+                                    {
+                                        cocktails.filter((cocktail) => cocktail.type == cocktailType.type).map((cocktail) => {
+                                            return (
+                                                <li className="cocktail-item">
+                                                    <div className="cocktail-name-cell">
+                                                        <span className="cocktail-name">{cocktail.name}</span>
+                                                        <br />
+                                                        <span className="cocktail-name-ja">{cocktail.nameJa}</span>
+                                                    </div>
+                                                    <div className="cocktail-price-cell">{`${cocktail.price.toLocaleString()} yen`}</div>
+                                                </li>
+                                            )
+                                        })
+                                    }
+                                    </ul>
+                                </div>
                             )
                         })
                     }
-                    </ul>
-                </div> 
+                </div>
                 <Footer />
             </>
         )
