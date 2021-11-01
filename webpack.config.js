68a7618c2423e83ce6c6173e8df96032a44c48bd
Yukitaka Maeda <yumaeda@gmail.com>
2021-11-01 21:51:34 +0900

    Use Sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path')
+const MODE = 'development'
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-    // mode: 'production',
-    mode: 'development',
+module.exports = [{
+    mode: MODE,
     entry: './src/index.tsx',
     module: {
         rules: [
@@ -31,4 +32,40 @@ module.exports = {
         filename: 'index.min.js',
         path: __dirname
     }
-}
+},
+
+{
+    mode: MODE,
+    entry: './scss/index.scss',
+    output: {
+        filename: 'index.css',
+        path: __dirname
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass')
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ]
+}]
