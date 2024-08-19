552d12cd6c86bb2fbfd8e63c0e324dc15b0af821
Yukitaka Maeda <yumaeda@gmail.com>
2024-08-19 22:53:13 +0900

    Show all the admin menus only to root user

--- src/components/pages/admin/HomeAdminPage.tsx
+++ src/components/pages/admin/HomeAdminPage.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import { USER_NAME_KEY } from '../../../constants/LocalStorageKeys'
+import RootAdminContents from './RootAdminContents'
  
 const HomeAdminPage: React.FC = () => {
-    return (
+    const userId = localStorage.getItem(USER_NAME_KEY) || ''
+    return (userId !== 'yumaeda') ? (
         <>
             <header className="admin-header">
                 <h1 className="admin-header-title">{`管理者ページ`}</h1>
@@ -10,12 +13,15 @@ const HomeAdminPage: React.FC = () => {
             <div className="admin-contents">
                 <ul>
                     <li><Link to="/admin/menu">メニューを管理</Link></li>
-                    <li><Link to="/admin/photo">写真を登録</Link></li>
-                    <li><Link to="/admin/restaurant-genre">レストランのジャンルを登録</Link></li>
-                    <li><Link to="/admin/restaurant-drink">レストランにお酒を登録</Link></li>
-                    <li><Link to="/admin/restaurant">レストランを登録</Link></li>
                 </ul>
-            </div> 
+            </div>
+        </>
+    ) : (
+        <>
+            <header className="admin-header">
+                <h1 className="admin-header-title">{`管理者ページ`}</h1>
+            </header>
+            <RootAdminContents />
         </>
     )
 }
